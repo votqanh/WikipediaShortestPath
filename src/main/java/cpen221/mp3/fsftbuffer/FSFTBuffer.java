97029ab6dc9c851792ba977cb3e7b153bb510dae
Anh Vo <votqanh@gmail.com>
2021-11-23 15:50:47 -0800

    Implemented FSFTBuffer

--- src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
+++ src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
@@ -1,4 +1,6 @@
 package cpen221.mp3.fsftbuffer;
+import java.lang.System;
+import java.util.*;
 
 public class FSFTBuffer<T extends Bufferable> {
 
@@ -7,6 +9,12 @@ public class FSFTBuffer<T extends Bufferable> {
 
     /* the default timeout value is 3600s */
     public static final int DTIMEOUT = 3600;
+
+    private final int capacity;
+    private final int timeout;
+    private int currentCapacity;
+
+    private final TreeMap<Long, ArrayList<T>> buffer = new TreeMap<>();
 
     /* TODO: Implement this datatype */
 
@@ -20,7 +28,9 @@ public class FSFTBuffer<T extends Bufferable> {
      *                 be in the buffer before it times out
      */
     public FSFTBuffer(int capacity, int timeout) {
-        // TODO: implement this constructor
+        this.capacity = capacity;
+        this.timeout = timeout;
+        this.currentCapacity = 0;
     }
 
     /**
@@ -36,8 +46,42 @@ public class FSFTBuffer<T extends Bufferable> {
      * object to make room for the new object.
      */
     public boolean put(T t) {
-        // TODO: implement this method
-        return false;
+        long currentTime = System.currentTimeMillis() / 1000;
+
+        Set<Long> times = new HashSet<>(buffer.keySet());
+
+        for (long time : times) {
+            if (time >= time + timeout) {
+                buffer.remove(time);
+                currentCapacity--;
+            } else {
+                break;
+            }
+        }
+
+        // remove least recently accessed
+        if (currentCapacity >= capacity) {
+            buffer.get(buffer.firstKey()).remove(0);
+        }
+
+        // if another object is added in the same second
+        for (long time : buffer.keySet()) {
+            if (time == currentTime) {
+                buffer.get(time).add(t);
+                currentCapacity++;
+                return true;
+            }
+        }
+
+        newTime(currentTime, t);
+        return true;
+    }
+
+    private void newTime(long currentTime, T t) {
+        ArrayList<T> l = new ArrayList<>();
+        l.add(t);
+        buffer.put(currentTime, l);
+        currentCapacity++;
     }
 
     /**
@@ -45,12 +89,20 @@ public class FSFTBuffer<T extends Bufferable> {
      * @return the object that matches the identifier from the
      * buffer
      */
-    public T get(String id) {
-        /* TODO: change this */
+    public T get(String id) throws NoSuchElementException {
         /* Do not return null. Throw a suitable checked exception when an object
             is not in the cache. You can add the checked exception to the method
             signature. */
-        return null;
+
+        for (long time : buffer.keySet()) {
+            for (T t : buffer.get(time)) {
+                if (Objects.equals(t.id(), id)) {
+                    return t;
+                }
+            }
+        }
+
+        throw new NoSuchElementException();
     }
 
     /**
@@ -62,7 +114,17 @@ public class FSFTBuffer<T extends Bufferable> {
      * @return true if successful and false otherwise
      */
     public boolean touch(String id) {
-        /* TODO: Implement this method */
+        Set<Long> times = new HashSet<>(buffer.keySet());
+
+        for (long time :times) {
+            for (T t : buffer.get(time)) {
+                if (Objects.equals(t.id(), id)) {
+                    buffer.get(time).remove(t);
+                    newTime(System.currentTimeMillis() / 1000, t);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -75,7 +137,17 @@ public class FSFTBuffer<T extends Bufferable> {
      * @return true if successful and false otherwise
      */
     public boolean update(T t) {
-        /* TODO: implement this method */
+        Set<Long> times = new HashSet<>(buffer.keySet());
+
+        for (long time :times) {
+            for (T t1 : buffer.get(time)) {
+                if (Objects.equals(t.id(), t1.id())) {
+                    buffer.get(time).remove(t);
+                    newTime(System.currentTimeMillis() / 1000, t);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
